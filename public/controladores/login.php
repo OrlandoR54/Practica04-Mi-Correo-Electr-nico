e9d512bdc9e29586ab177e4b8154f69b927880c0
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-11-24 22:38:33 -0500

    Update login.php

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -1,20 +1,36 @@
-<?php
-    session_start();
 
-    include '../../config/conexionBD.php';
-
-    $usuario= isset($_POST["email"])?trim($_POST["email"]):null;
-    $contrasena= isset($_POST["password"])?trim($_POST["password"]):null;
-
-    $sql = "SELECT*FROM usuario WHERE usu_correo='$usuario'and usu_password=MD5('$contrasena')";
-
-    $result = $conn->query($sql);    
-    if ($result->num_rows>0) {
-        $_SESSION['isLogged']=TRUE;
-        header("Location:../../admin/vista/usuario/index.php");
-    }else {
+<?php 
+    session_start(); 
+ 
+    include '../../config/conexionDB.php'; 
+ 
+    $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null; 
+    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null; 
+ 
+    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = 
+    MD5('$contrasena')"; 
+ //'$contrasena'"; 
+    $result = $conn->query($sql);
+    $fila=$result->fetch_assoc();//obteniendo los resultados de la consulta     
+    if ($result->num_rows > 0) {         
+        $_SESSION['isLogged'] = TRUE;//sesion iniciada
+        $u_codigo=$fila['usu_codigo'];
+        $u_nombre=$fila['usu_nombre'];
         
-        header("Location:../vista/login.html");
-    }
-    $conn->close();
-?>
+        if($fila['u_rol']=='U'){//ES UN USUARIO
+            
+            header("Location: ../../admin/vista/user/index.php?usu_codigo=$u_codigo&usu_nombre=$u_nombre");   
+        }else{//ES ADministrador
+            header("Location: ../../admin/vista/admin/index.php?usu_codigo=$u_codigo&usu_nombre=$u_nombre");
+        }
+        
+       
+    } else { //login fallido 
+           
+        header("Location: ../vista/login.html"); 
+        //echo"<p> USUARIO " .$sql. " </p>"; 
+    } 
+         
+    $conn->close(); 
+ 
+?> 
