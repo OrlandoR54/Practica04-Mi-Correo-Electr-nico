e812f04bdf796b9ddec7ae0b5b181b55d70a4331
OrlandoR54 <46764632+OrlandoR54@users.noreply.github.com>
2019-11-27 11:00:10 -0500

    Update crear_usuario.php

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -6,36 +6,35 @@
     <title>Crear usuario</title>
 </head>
 <body>
-    <?php
+    <form class="box">
+        <?php
+        //Incluir conexion a la base de datos
         include '../../config/conexionBD.php';
 
-        $cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]):null;
-        $nombre = isset($_POST["nombre"]) ? mb_strtoupper(trim($_POST["nombre"]),'UTF-8'):null;
-        $apellido = isset($_POST["apellido"]) ? mb_strtoupper(trim($_POST["apellido"]), 'UTF-8'): null;
-        $direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8'): null;
-        $telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]):null;
-        $correo = isset($_POST["email"]) ? trim($_POST["email"]):null;
-        $fechaNacimiento = isset($_POST["fecha"]) ? trim($_POST["fecha"]):null;
-        $contrasena = isset($_POST["password"]) ? trim($_POST["password"]):null;
-
-        $sql = "INSERT INTO usuario VALUES (0, '$cedula','$nombre','$apellido','$direccion','$telefono','$correo', MD5('$contrasena'),'$fechaNacimiento','N',null,null,'user')";
-
-        if ($conn->query($sql)===TRUE) {
-            echo "<p>Se ha creado los datos personales correctamente</p>";
-            header( "Location: ../vista/login.html");
-        }else {
+        $cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]) : null;
+        $nombres = isset($_POST["nombre"]) ? mb_strtoupper(trim($_POST["nombre"]), 'UTF-8') : null;
+        $apellidos = isset($_POST["apellido"]) ? mb_strtoupper(trim($_POST["apellido"]), 'UTF-8') : null;
+        $direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8') : null;
+        $telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]) : null;
+        $correo = isset($_POST["email"]) ? trim($_POST["email"]) : null;
+        $fechaNacimiento = isset($_POST["fecha"]) ? trim($_POST["fecha"]) : null;
+        $contrasena = isset($_POST["password"]) ? trim($_POST["password"]) : null;
+        $sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion', '$telefono', '$correo', MD5('$contrasena'), '$fechaNacimiento', 'N', null, null, 2)";
+        if ($conn->query($sql) == TRUE) {
+            echo "<p>Se han creado los datos personales correctamente!!!</p>";
+        } else {
             if ($conn->errno == 1062) {
-                echo "<p class='error'>La persona con la cedula $cedula ya esta en el sistema</p>";
-                
-
-            }else {
-                echo "<p class='error'>Error.".mysqli_error($conn)."</p>";
-                echo "<a href='../vista/registrarse.html'>Volver a Registro</a>";
+                echo "<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema</p>";
+            } else {
+                echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
             }
         }
-        //Cerrar la base de datos
         $conn->close();
-        echo "<a href='../vista/crear_usuario.html'>Regresar</a>";
-    ?>
+        echo "  <div class="botones">
+                    <input calss="boton" type="submit" value="Iniciar Sesion" onclick="history.back()">
+                </div>";
+        ?>
+    </form>
 </body>
+
 </html>
